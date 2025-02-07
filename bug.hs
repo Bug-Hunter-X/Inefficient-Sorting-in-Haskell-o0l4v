bd26d8b77463a437f92f761dd3948222c13cfbf5
BugHunter-X <askzenai@gmail.com>
2025-02-07 06:46:05 +0100

    Add bug.hs file

---
+++ bug.hs
@@ -0,0 +1,11 @@
+```haskell
+import Data.List (sort)
+
+main :: IO ()
+main = do
+  let xs = [1, 2, 3, 4, 5]
+  let ys = sort xs
+  print ys
+
+-- This code is correct, but it doesn't demonstrate a particularly uncommon error.
+```
